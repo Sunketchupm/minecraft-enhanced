e4a187437a81d2019526db44ade9826c21c140ba
DaMemes2 <yachenyao123@gmail.com>
2026-04-06 00:16:07 +0800

    Music ports extended to 8 tracks.

--- plot.lua
+++ plot.lua
@@ -44,11 +44,17 @@ local function musicplot(msg) -- Various music for plots, can be used to add mor
     elseif msg == "6" then djui_chat_message_create("Music: Touhou 9: Phantasmagoria of Flower View - Ghostly Band ~ Phantom Ensemble (Touhoumon Version). Author: DaMemes")
         smlua_audio_utils_replace_sequence(0x64, 0x25, 75, "08_Seq_touhoumonprismriver_custom")
         return true
+    elseif msg == "7" then djui_chat_message_create("Music: Touhou 16: Hidden Star in Four Seasons - Illusionary White Traveler. Author: DaMemes")
+        smlua_audio_utils_replace_sequence(0x64, 0x25, 75, "09_Seq_th16stage4_custom")
+        return true
+    elseif msg == "8" then djui_chat_message_create("Music: Touhou 16: Hidden Star in Four Seasons - Secret God Matara ~ Hidden Star in All Seasons. Author: DaMemes")
+        smlua_audio_utils_replace_sequence(0x64, 0x25, 75, "0A_Seq_th16okina2_custom")
+        return true
     end
     return false
 end
 
-hook_chat_command("plot-music", "[1-6] Changes music that plays in plots (will display port author as well). Re-enter plot if you have decided to change the song while you're in it.", musicplot)
+hook_chat_command("plot-music", "[1-8] Changes music that plays in plots (will display port author as well). Re-enter plot if you have decided to change the song while you're in it.", musicplot)
 hook_chat_command("plot", "| Warp to a level with no textures or objects. Pass an argument to go to a specific act.", on_chat_command)
 hook_event(HOOK_ON_LEVEL_INIT, on_lvl_init)
 hook_event(HOOK_ON_PAUSE_EXIT, on_pause_exit)
